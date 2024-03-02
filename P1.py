65dc51073fc401137146bd1967ffe0966a9ae888
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-02 17:42:32 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -2,6 +2,6 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.markdown('# Parcial 1\n Laboratorio de reducción de datos\nDessiré Zapeta Hernández\n 202112959')
+st.markdown('# Parcial 1\n Laboratorio de reducción de datos\n Dessiré Zapeta Hernández\n 202112959')
 
 st.button("Reset", type="primary")

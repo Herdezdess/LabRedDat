d1b52a18b0f357ccfb0830e395f7a86035e17610
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-03 10:03:10 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -8,7 +8,10 @@ st.write('Laboratorio de reducción de datos')
 st.write('Dessiré Zapeta Hernández')
 st.write('202112959')
 
-n = st.text_input('Valor de n')
+n = st.text_input('Ingrese el valor deseado de n')
 st.write('El valor de n es ', n)
+
+p = st.text_input('Ingrese el valor deseado de p')
+st.write('El valor de n es ', p)
 
 st.button("Reset", type="primary")

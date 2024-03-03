97166110a43c610f925bd9611ff02b8746425d61
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-03 09:51:58 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -2,11 +2,11 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.header('Parcial 1')
 st.title('Parcial 1')
 
 st.write('Laboratorio de reducción de datos')
 st.write('Dessiré Zapeta Hernández')
 st.write('202112959')
 
+n = input('Ingrese el valor de n')
 st.button("Reset", type="primary")

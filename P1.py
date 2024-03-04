a33bf0de6145b1f3d63966f5eaac42cb6bccc463
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-03 21:51:56 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -15,11 +15,11 @@ st.write('202112959')
 n = st.number_input("Ingrese el valor deseado de n", value=None, placeholder="Type a number...")
 #posibles errores, si n no es entero, si n no es un numero
 if  (isinstance(n, int)==false):
-st.write('n debe ser un número entero.')
+    st.write('n debe ser un número entero.')
 else if (n.isdigit()==false):
-st.write('n debe ser un valor númerico')
+    st.write('n debe ser un valor númerico')
 else
-st.write('El valor de n es ', n)
+    st.write('El valor de n es ', n)
 
 
 #Definimos los errores
@@ -28,11 +28,11 @@ st.write('El valor de n es ', n)
 p = st.number_input("Ingrese el valor deseado de p", value=None, placeholder="Type a number...")
 
 if (p>1):
-st.write("P debe ser un número menor que 1")
+    st.write("P debe ser un número menor que 1")
 else if (p.isdigit()==false):
-st.write('p debe ser un valor númerico')
+    st.write('p debe ser un valor númerico')
 else
-st.write('El valor de n es ', p)
+    st.write('El valor de n es ', p)
 
 
 

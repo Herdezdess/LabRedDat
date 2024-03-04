f1da704b724dafad44c502813d0ee54c880d6060
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-03 22:09:09 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -15,6 +15,7 @@ st.write('202112959')
 n = st.number_input("Ingrese el valor deseado de n", value=None, placeholder="Type a number...")
 
 #posibles errores, si n no es entero, si n no es un numero
+#No sé porqué aquí ya no me funcionó el código, pidoperdón yo solo sé javascript
 #if  (isinstance(n, int)==False):
  #   st.write('n debe ser un número entero.')
 #elif (n.isdigit()==False):
@@ -23,23 +24,14 @@ n = st.number_input("Ingrese el valor deseado de n", value=None, placeholder="Ty
  #   st.write('El valor de n es ', n)
 
 
-
-
-
 p = st.number_input("Ingrese el valor deseado de p", value=None, placeholder="Type a number...")
-
+#Posibles errores de p
 #if (p>1):
 #    st.write("P debe ser un número menor que 1")
 #elif (p.isdigit()==False):
 #    st.write('p debe ser un valor númerico')
 #else:
 #    st.write('El valor de n es ', p)
-
-
-
-#Posibles errores de p
-
-
 
 
 #Definimos la función binomial
@@ -54,7 +46,7 @@ lista = np.arange(n+1)
 
 #Definimos la primera fila de la tabla
 data_table = pd.DataFrame({'x':lista})
-#Ponelos la tabla con el valor probabilistico
+#Ponemos la tabla con el valor probabilistico
 data_table['Pb'] = data_table.apply(lambda row: binomial(row['x'],n,p), axis=1)
 st.write(data_table)
 
@@ -63,7 +55,7 @@ binomial_plot, axis = plt.subplots()
 axis.bar(data_table['x'],data_table['Pb'])
 axis.plot(data_table['x'],data_table['Pb'],color='C1')
 binomial_plot.savefig('imagen.png')
-#Le ponemos título al gráfico
+#Le ponemos título al gráfico y mostramos el gráfico
 st.title('Graficos binomiales')
 st.pyplot(binomial_plot)
 

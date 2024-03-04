468301ada89d6627b86233e7760eccca0916bf3a
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-03-03 20:38:06 -0600

    Update P1.py

--- P1.py
+++ P1.py
@@ -12,9 +12,9 @@ st.write('Dessiré Zapeta Hernández')
 st.write('202112959')
 
 #Le pedimos al usuario que ingrese los datos necesarios
-n = st.text_input('Ingrese el valor deseado de n, el número intentos', '1')
+n = st.text_input('Ingrese el valor deseado de n', '1')
 st.write('El valor de n es ', n)
-p = st.text_input('Ingrese el valor deseado de p, la probabilidad de obtener el caso deseado', '1/2')
+p = st.text_input('Ingrese el valor deseado de p', '1/2')
 st.write('El valor de p es ', p)
 
 #Definimos la función binomial

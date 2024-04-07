b6a31e84809afd84a84f3568457d24459b00965a
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-04-06 20:12:42 -0600

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -2,3 +2,18 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="Práctica 2: Predicción de COVID19", page icon="🌍", layout="wide")
+with st.sidebar:
+  selected=option_menu(
+    menu_title="Menú",
+    options = ["Principal", "Teoría"],
+    icons = ["house-heart-fill", "envelope-heart_fill"],
+    menu_icon = "heart-eyes-fill",
+    default_index = 0,
+  )
+if selected == "Principal":
+  st.markdown("hola")
+
+if selected == "Teoría":
+  st.markdown("hola 2")

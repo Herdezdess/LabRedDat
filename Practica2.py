8a9adb9ec7eb09c39bf5d5c50ffd19f30d3b58ed
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-04-07 15:09:30 -0600

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -4,6 +4,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 import plotly.express as px
+import plotly.graph_objects as go
+import os, time
 
 st.set_page_config(page_title="Práctica 2: Predicción de COVID19", page_icon="🌍", layout="wide")
 with st.sidebar:
@@ -15,16 +17,12 @@ with st.sidebar:
     default_index = 0,
   )
 if selected == "Principal":
-  #título
+#título
   st.markdown("<h1 style='text-align: center; color: #A2BDF1;'>Predicción de COVID19</h1>", unsafe_allow_html=True)
-  #lector del csv
+#lector del csv
   data = pd.read_csv('https://raw.githubusercontent.com/Herdezdess/LabRedDat/main/confirmados_fecha.csv')
-  data['Fecha'] = pd.to_datetime(data['Fecha'])
-  fig = px.line(data, x='fecha', y='Casos por fecha de inicio de síntomas', title='Número de casos confirmados por fecha de inicio de síntomas')
-  fig.update_xaxes(title='Fecha')
-  fig.update_yaxes(title='Casos')
-  st.plotly_chart(fig)
-  
+  print(data)
+
   
 
 if selected == "Teoría":

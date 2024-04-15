60371a6538c5a1a61310ba37843dec606fd64852
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2024-04-14 22:23:11 -0600

    Update Practica2.py

--- Practicas/Practica2.py
+++ Practicas/Practica2.py
@@ -287,6 +287,11 @@ if selected == "Principal":
   
 
 if selected == "Teoría":
+  st.divider()
+  st.markdown("""Práctica 2. Laboratorio de reducción de datos.""")
+  st.markdown("""Mencos Avila, Allan Fabricio. 202106009""")
+  st.markdown("""Zapeta Hernández, Alejandra Dessiré. 202112959""")
+  st.divider()
   st.markdown("<h1 style='text-align: center; color: #A2BDF1; text-shadow: 3px 3px #BEFBB3;'>--- RESUMEN ---</h1>", unsafe_allow_html=True)
   st.markdown("""La pandemia del COVID-19 fué uno de los desafíos más grandes a los que la sociedad guatemalteca se ha enfrentado en la historia reciente. Esto no afectó solamente al sector de la salud, sino que fue un problema multifacético el cual afectó principalmente en las siguientes áreas:""")
   st.markdown("""  
